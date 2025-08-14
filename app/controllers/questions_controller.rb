d6e1f8b09d8a626bec1207d25179408b099fe888
ushiyushi <ymurae46@gmail.com>
2025-08-14 11:34:15 +0900

    投稿フォームの修正

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,31 @@
 class QuestionsController < ApplicationController
   before_action :move_to_index, except: [:index]
 
-  def index; end
+  def index
+    @questions = Question.all
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = current_user.questions.build(question_params)
+    if @question.save
+      redirect_to root_path
+    else
+      logger.debug "Question save failed. Errors: #{@question.errors.full_messages.inspect}"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
+
+  def question_params
+    params.require(:question).permit(:title, :text, :category_id, :deadline)
+  end
+
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,7 @@
 class Question < ApplicationRecord
+  validates :title, presence: true
+  validates :text, presence: true
+
   belongs_to :user
   has_many :comments
 
